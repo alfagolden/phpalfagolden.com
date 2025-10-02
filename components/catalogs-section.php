33d795f00bb3f4cf98828d28ebf9b04dec9a4af1
A7SNcom <you@example.com>
2025-10-02 17:09:25 +0000

    autosync: 2025-10-02T17:09:25Z

--- components/catalogs-section.php
+++ components/catalogs-section.php
@@ -61,7 +61,7 @@ function fetchCatalogsFromBase($tableId) {
     global $API_CONFIG;
     try {
         // جلب السجلات مع فلترة الحالة (افتراضياً 'active' أو 'نشط'، قم بتعديل حسب القيم الفعلية)
-        $response = makeApiRequest("rows/table/{$tableId}/user_field_names=true/?filter_field_6756__equal=كتلوجات"); // فلت عالة////////
+        $response = makeApiRequest("rows/table/{$tableId}/user_field_names=false/?filter_field_6756__equal=كتلوجات"); // فلت عالة////////
         if (!$response || !isset($response['results'])) {
             return [];
         }
