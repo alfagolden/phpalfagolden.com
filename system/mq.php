9764f63992777eefffc74b0e3ad13f95e441e580
A7SNcom <you@example.com>
2025-10-04 21:22:14 +0000

    autosync: 2025-10-04T21:22:14Z

--- system/mq.php
+++ system/mq.php
@@ -638,7 +638,7 @@ usort($filteredQuotes, function($a, $b) use ($FIELDS, $sortBy, $sortDir) {
                                 ?>
                                 <tr class="table-row">
                                     <td class="table-cell">
-                                        <div class="quote-id">#<?= convertToEnglishNumbers(number($quoteNumber)) ?></div>
+                                        <div class="quote-id">#<?= convertToEnglishNumbers( $quoteNumber) ?></div>
                                     </td>
                                     <td class="table-cell">
                                         <div class="quote-date"><?= $quoteDate ?></div>
